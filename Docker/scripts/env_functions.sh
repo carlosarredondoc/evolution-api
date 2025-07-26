896b811fae7ab804fe78c578dd08b5a557666891
Carlos Arredondo <63679967+carlosarredondoc@users.noreply.github.com>
2025-07-26 01:14:57 -0400

    Update env_functions.sh

--- Docker/scripts/env_functions.sh
+++ Docker/scripts/env_functions.sh
@@ -1,15 +1,20 @@
 export_env_vars() {
     if [ -f .env ]; then
         while IFS='=' read -r key value; do
-            if [[ -z "$key" || "$key" =~ ^\s*# || -z "$value" ]]; then
+            # Ignora comentarios y líneas vacías
+            if [[ -z "$key" || "$key" =~ ^\s*# ]]; then
                 continue
             fi
 
+            # Limpia la clave de espacios
             key=$(echo "$key" | tr -d '[:space:]')
-            value=$(echo "$value" | tr -d '[:space:]')
-            value=$(echo "$value" | tr -d "'" | tr -d "\"")
-
-            export "$key=$value"
+            
+            # Revisa si la variable ya existe en el entorno y tiene un valor
+            if [ -z "${!key}" ]; then
+                # Si no existe, la procesa y la exporta
+                value=$(echo "$value" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e "s/^'//" -e "s/'$//" -e 's/^"//' -e 's/"$//')
+                export "$key=$value"
+            fi
         done < .env
     fi
 }
